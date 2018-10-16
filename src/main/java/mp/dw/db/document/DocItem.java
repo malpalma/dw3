5d2f777dcbbe3b720bcf3ccbc3dbcc50ce65462c
Małgorzata Palma <Magorzata Palma@old-one>
2018-10-16 14:58:16 +0200

    change in DocItem.id generation strategy

--- src/main/java/mp/dw/db/document/DocItem.java
+++ src/main/java/mp/dw/db/document/DocItem.java
@@ -21,7 +21,7 @@ import org.hibernate.annotations.FetchMode;
 public class DocItem {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@ManyToOne
