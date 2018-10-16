253553de7f45d76c0668a24e19aaf59b0d5f25a2
Małgorzata Palma <Magorzata Palma@old-one>
2018-10-16 14:53:22 +0200

    change in Document.id generation strategy

--- src/main/java/mp/dw/db/document/Document.java
+++ src/main/java/mp/dw/db/document/Document.java
@@ -33,7 +33,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Document {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column(nullable = false)
